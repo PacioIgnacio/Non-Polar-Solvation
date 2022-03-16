c08d577c5cfa01cf2b0629de069f4ad51dbbb14f
PacioIgnacio <ignamunozq@gmail.com>
2022-03-16 14:53:14 -0300

    9ALA Molecular Dynamics Preamble

---
+++ 09ALA/extended/TI/slurm_example15.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#!/bin/sh
+#SBATCH -n 2
+#SBATCH --job-name=9A_TI
+#SBATCH --output=9ALA-%A_%a.out
+#SBATCH --error=9ALA-%A_%a.err
+#SBATCH --ntasks-per-node=2
+#SBATCH --partition=intel
+##SBATCH --mail-user=ignacio.siao@gmail.com
+##SBATCH --mail-type=ALL
+#SBATCH --array=1-1%1
+#SBATCH --mem-per-cpu=2G
+
+
+module load md++
+
+export OMP_NUM_THREADS=2
+
+INFILE=`awk "NR==$SLURM_ARRAY_TASK_ID" submit15.txt`
+source $INFILE
