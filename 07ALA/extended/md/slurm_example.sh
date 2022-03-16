4f26a2fd2a821eda81c87e2ff1e6f9f974e79d2c
PacioIgnacio <ignamunozq@gmail.com>
2022-03-16 14:03:09 -0300

    7ALA Molecular Dynamics Preamble

---
+++ 07ALA/extended/md/slurm_example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#!/bin/sh
+#SBATCH -n 2
+#SBATCH --job-name=7ALAmd
+#SBATCH --output=7ALA-%A_%a.out
+#SBATCH --error=7ALA-%A_%a.err
+#SBATCH --ntasks-per-node=2
+#SBATCH --partition=intel
+##SBATCH --mail-user=ignacio.siao@gmail.com
+##SBATCH --mail-type=ALL
+#SBATCH --array=1-1%1
+#SBATCH --mem-per-cpu=2G
+
+
+module load md++
+
+export OMP_NUM_THREADS=2
+
+INFILE=`awk "NR==$SLURM_ARRAY_TASK_ID" submit.txt`
+source $INFILE
