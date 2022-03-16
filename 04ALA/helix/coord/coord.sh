ee8f0417c86ec158b27c3280a5d9d337ccef55a0
PacioIgnacio <ignamunozq@gmail.com>
2022-03-16 10:28:38 -0300

    4ALA Molecular Dynamics Preamble

---
+++ 04ALA/helix/coord/coord.sh
@@ -0,0 +1,10 @@
+  # !/usr/bin/bash
+#
+for i in 1 2 3 4 5 6 7 8 9
+do
+   echo 0$iALA
+   cd /home/imunoz/NPT.Lambda/0$iALA/helix/coord
+   pdb2g96 @f pdb2g96_peptide.arg > pdb2g96_4ALA.cnf
+   gch @f gch_peptide.arg > gch_peptide.cnf
+   echo 0$iALA
+doneexit
